a0ffd96d8fa5ecf35792a2b478d4db3608f5d03b
seki <102042736+HERUESTA@users.noreply.github.com>
2025-08-11 15:41:56 +0900

    requestSpecの一部を削除

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -40,13 +40,6 @@ RSpec.describe "/books", type: :request do
     end
   end
 
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_book_url
-      expect(response).to be_successful
-    end
-  end
-
   describe "GET /edit" do
     it "renders a successful response" do
       book = Book.create! valid_attributes
