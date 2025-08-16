ff27ac91d79579b5b42aca9343128f097c79d9cf
m-porun <butachikusho@gmail.com>
2025-08-16 10:37:53 +0900

    [ADD]現状システムテストが動く状態

---
+++ spec/system/books_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Books", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe "createアクション" do
+    it "新しい本が正常に作成される" do
+      visit new_book_path
+
+      expect(page).to have_content("新しい本の作成")
+      
+      fill_in "Title", with: "テスト本のタイトル"
+      fill_in "Author", with: "テスト著者"
+
+      expect {
+        click_button "Create Book"
+      }.to change(Book, :count).by(1)
+
+      expect(page).to have_content("本が作成されました。")
+      expect(page).to have_content("テスト本のタイトル")
+      expect(page).to have_content("テスト著者")
+    end
+  end
+end
+
